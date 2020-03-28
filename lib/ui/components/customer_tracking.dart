7bfbb3b8e04c9fe5983635b669b9d4bc70bfd1ea
Nils Müller <mueller_nils95@yahoo.de>
2020-03-28 23:55:50 +0100

    Alert für input

--- lib/ui/components/customer_tracking.dart
+++ lib/ui/components/customer_tracking.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 class CustomerTracking extends StatefulWidget {
@@ -10,6 +11,7 @@ class _CustomerTrackingState extends State<CustomerTracking> {
   int customers = 0;
   int limit = 10;
   int queue = 0;
+  String storeName = 'Coop Europaplatz';
 
   var activeButton = [false, false, false];
   bool queueEnabled = false;
@@ -17,63 +19,138 @@ class _CustomerTrackingState extends State<CustomerTracking> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Text(
-          'Kundentracking',
+          '$storeName',
           style: TextStyle(
             fontSize: 44,
           ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$customers',
-              style: TextStyle(
-                fontSize: 76,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '($queue)',
-              style: TextStyle(
-                fontSize: 36,
-              ),
-            ),
-          ],
+        Text(
+          '$customers',
+          style: TextStyle(
+            fontSize: 200,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-              color: Colors.blue,
-              child: Text('-'),
-              onPressed: () {
-                decreaseCustomerCount();
-              },
+            buildCountButton('-', () {
+              decreaseCustomerCount();
+            }),
+            SizedBox(
+              width: 20,
             ),
-            FlatButton(
-              color: Colors.red,
-              child: Text('+'),
-              onPressed: () {
-                increaseCustomerCount();
-              },
-            )
+            buildCountButton('+', () {
+              increaseCustomerCount();
+            }),
           ],
         ),
-        Text('Warteschlange'),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          'Warteschlange',
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
         Padding(
-          padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.only(
+            left: 50,
+            right: 50,
+          ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              buildQueueButton(0, '5', 5),
-              buildQueueButton(1, '10', 10),
-              buildQueueButton(2, '15+', 15),
+              buildQueueButton(0, 'Kurz', 5),
+              buildQueueButton(1, 'Mittel', 10),
+              buildQueueButton(2, 'Lang', 15),
             ],
           ),
-        )
+        ),
+        InkWell(
+          child: Text(
+            'Ladenkapazität anpassen',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 16,
+            ),
+          ),
+          onTap: () {
+            createDialog(context).then((newLimit) {
+              if (customers > newLimit) {
+                setState(() {
+                  customers = newLimit;
+                  queueEnabled = true;
+                });
+              } else {
+                setState(() {
+                  queueEnabled = false;
+                  activeButton = [false, false, false];
+                });
+              }
+              setState(() {
+                limit = newLimit;
+              });
+            });
+          },
+        ),
       ],
+    );
+  }
+
+  Future<int> createDialog(BuildContext context) {
+    TextEditingController customController =
+        TextEditingController(text: limit.toString());
+
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Ladenkapazität'),
+            content: TextField(
+              decoration: InputDecoration(
+                labelText: "Geschäftskapazität",
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                WhitelistingTextInputFormatter.digitsOnly,
+              ], // O
+              controller: customController,
+            ),
+            actions: <Widget>[
+              MaterialButton(
+                child: Text('Speichern'),
+                onPressed: () {
+                  final newLimit = int.parse(customController.text.toString());
+                  Navigator.of(context).pop(newLimit);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  ButtonTheme buildCountButton(String text, VoidCallback onPressedAction) {
+    return ButtonTheme(
+      minWidth: 100,
+      height: 100,
+      child: FlatButton(
+        color: Colors.grey[200],
+        shape: new RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(10.0),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 50,
+          ),
+        ),
+        onPressed: onPressedAction,
+      ),
     );
   }
 
