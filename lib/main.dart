3b6282dde2338cf89d3dcad91633e4ab034dd050
Luecu <luca.indermuehle@gmail.com>
2020-03-28 17:16:57 +0100

    feat: add state provider DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:waitathome/core/service/shop_service.dart';
 import 'package:waitathome/ui/screen/customer/customer_screen.dart';
 import 'package:waitathome/ui/screen/intro_screen.dart';
 import 'package:waitathome/ui/screen/shop/shop_screen.dart';
@@ -8,14 +10,17 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => IntroScreen(),
-        IntroScreen.routeName: (context) => IntroScreen(),
-        CustomerScreen.routeName: (context) => CustomerScreen(),
-        ShopScreen.routeName: (context) => ShopScreen(),
-      },
+    return Provider(
+      create: (_) => new ShopService(),
+      child: MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => IntroScreen(),
+          IntroScreen.routeName: (context) => IntroScreen(),
+          CustomerScreen.routeName: (context) => CustomerScreen(),
+          ShopScreen.routeName: (context) => ShopScreen(),
+        },
+      ),
     );
   }
 }
