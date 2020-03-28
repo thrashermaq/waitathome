c0b0d7a73a3fd87ad83edce138ad017b366c7dee
Ralf Winkelmann <ralf.winkelmann@hotmail.com>
2020-03-28 20:22:04 +0100

    - Refactoring & custom info widget for marker

---
+++ lib/ui/components/info_widget.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:waitathome/core/model/marker_data.dart';
+
+class InfoWidget extends StatefulWidget {
+  double position;
+  MarkerData markerData;
+
+  InfoWidget({this.position, this.markerData});
+
+  @override
+  _InfoWidgetState createState() => _InfoWidgetState();
+}
+
+class _InfoWidgetState extends State<InfoWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPositioned(
+      bottom: widget.position,
+      right: 0,
+      left: 0,
+      duration: Duration(milliseconds: 200),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          margin: EdgeInsets.all(20),
+          height: 70,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  blurRadius: 20,
+                  offset: Offset.zero,
+                  color: Colors.grey.withOpacity(0.5),
+                )
+              ]),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          widget.markerData.shopName,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Personen: ${widget.markerData.peopleCount}, Schlange: ${widget.markerData.queueCount}',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    widget.markerData.isFavourite
+                        ? Icons.star
+                        : Icons.star_border,
+                    color: Colors.orange,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      widget.markerData.isFavourite =
+                          !widget.markerData.isFavourite;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/screen/customer/map_screen.dart
+++ lib/ui/screen/customer/map_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:waitathome/core/model/marker_data.dart';
+import 'package:waitathome/ui/components/info_widget.dart';
 
 const double CAMERA_ZOOM = 15;
-const LatLng START_LOCATION = LatLng(42.7477863, -71.1699932);
+const LatLng START_LOCATION = LatLng(46.94809, 7.44744);
 
 /// TODO: Package for search: https://pub.dev/packages/search_map_place
 class MapScreen extends StatefulWidget {
@@ -20,6 +22,15 @@ class _MapScreenState extends State<MapScreen> {
 
   final LatLng _center = START_LOCATION;
   Set<Marker> _markers = new Set();
+
+  double pinPillPosition = -100;
+
+  MarkerData selectedPin = MarkerData(
+      shopName: 'Coop Bern',
+      statusColor: Colors.orange,
+      peopleCount: 37,
+      queueCount: 0,
+      isFavourite: false);
 
   @override
   void initState() {
@@ -52,13 +63,29 @@ class _MapScreenState extends State<MapScreen> {
                   markerId: MarkerId('1'),
                   position: LatLng(46.94809, 7.44744),
                   icon: markerIcon,
+                  onTap: () {
+                    setState(() {
+                      // TODO: selectedPin = sourcePinInfo;
+                      pinPillPosition = 0;
+                    });
+                  },
                 ));
+              });
+            },
+            onTap: (LatLng location) {
+              // reset info widget
+              setState(() {
+                pinPillPosition = -100;
               });
             },
             initialCameraPosition: CameraPosition(
               target: _center,
               zoom: CAMERA_ZOOM,
             ),
+          ),
+          InfoWidget(
+            position: pinPillPosition,
+            markerData: selectedPin,
           ),
         ],
       ),
