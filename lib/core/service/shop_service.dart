b2a8db7336de34e8df9e9425ea70fd6e76349800
Luecu <luca.indermuehle@gmail.com>
2020-03-28 19:08:52 +0100

    feat: add login to shop service

--- lib/core/service/shop_service.dart
+++ lib/core/service/shop_service.dart
@@ -8,6 +8,20 @@ class ShopService {
     this.databaseReference = Firestore.instance;
   }
 
+  login(String loginCode, void onLoginSuccessful(String shopId), void onLoginFailed()) {
+    Firestore.instance
+        .collection("shop-codes")
+        .document(loginCode)
+        .get()
+        .then((value) {
+          if (value.data == null) {
+            onLoginFailed();
+          } else {
+            onLoginSuccessful(value.data["shop-id"]);
+          }
+    });
+  }
+
   void getShop(String id, void onShop(Shop event)) {
     databaseReference
         .collection("shops")
