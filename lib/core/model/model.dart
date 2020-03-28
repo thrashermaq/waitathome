9b1f159577d6282b02f3e80bde0b138a498c8b56
Ralf Winkelmann <ralf.winkelmann@hotmail.com>
2020-03-28 13:18:48 +0100

    Added showcase model and service class

---
+++ lib/core/model/model.dart
@@ -0,0 +1,7 @@
+/// TODO: Remove. Just for showcase
+class Model {
+  int id;
+  String name;
+
+  Model(this.id, this.name);
+}
---
+++ lib/core/service/service.dart
@@ -0,0 +1,6 @@
+import 'package:waitathome/core/model/model.dart';
+
+/// TODO: Remove. Just for showcase
+class Service {
+  static Model getModel() => Model(10, 'Test');
+}
