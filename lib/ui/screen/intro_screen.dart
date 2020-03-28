6ca29e51ce0d1d67fcd127d9a974d3d6d7f3b910
Ralf Winkelmann <ralf.winkelmann@hotmail.com>
2020-03-28 13:34:02 +0100

    Navigation

--- lib/ui/screen/intro_screen.dart
+++ lib/ui/screen/intro_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:waitathome/ui/components/custom_button.dart';
+import 'package:waitathome/ui/screen/customer/customer_screen.dart';
+import 'package:waitathome/ui/screen/shop/shop_screen.dart';
 
 class IntroScreen extends StatelessWidget {
   static const routeName = '/intro';
@@ -13,11 +15,15 @@ class IntroScreen extends StatelessWidget {
           children: <Widget>[
             CustomButton(
               label: 'Shop',
-              onPressed: () {},
+              onPressed: () {
+                Navigator.pushNamed(context, ShopScreen.routeName);
+              },
             ),
             CustomButton(
               label: 'Customer',
-              onPressed: () {},
+              onPressed: () {
+                Navigator.pushNamed(context, CustomerScreen.routeName);
+              },
             ),
           ],
         ),
