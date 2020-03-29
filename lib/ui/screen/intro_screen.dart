36a03898eeee2456d3ce7acd4a46a002332005ce
Hoang Tran <hoang.tran@sbb.ch>
2020-03-29 13:52:43 +0200

    updated intro screen

--- lib/ui/screen/intro_screen.dart
+++ lib/ui/screen/intro_screen.dart
@@ -24,76 +24,22 @@ class IntroScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                Container(
-                  height: 48.0,
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(16.0),
-                  child: FlatButton(
-                    color: Colors.teal,
-                    textColor: Colors.white,
-                    padding: EdgeInsets.all(8.0),
+                Padding(
+                  padding: EdgeInsets.only(top: 48.0, bottom: 16.0),
+                  child: _IntroScreenButton(
+                    label: 'Shop associate',
+                    imagePath: 'assets/images/intro_shop.png',
                     onPressed: () {
                       Navigator.pushNamed(context, ShopLoginScreen.routeName);
                     },
-                    child: Row(
-                      children: <Widget>[
-                        Image(
-                          image: AssetImage('assets/images/intro_shop.png'),
-                          height: 115,
-                          width: 115,
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text(
-                              "Shop associate",
-                              style: TextStyle(
-                                fontSize: 36.0,
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w100,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
                   ),
                 ),
-                Container(
-                  height: 16.0,
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(16.0),
-                  child: FlatButton(
-                    color: Colors.teal,
-                    textColor: Colors.white,
-                    padding: EdgeInsets.all(8.0),
-                    onPressed: () {
-                      Navigator.pushNamed(context, MapScreen.routeName);
-                    },
-                    child: Row(
-                      children: <Widget>[
-                        Image(
-                          image: AssetImage('assets/images/intro_customer.png'),
-                          height: 115,
-                          width: 115,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            "Customer",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 36.0,
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w100,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                _IntroScreenButton(
+                  label: 'Customer',
+                  imagePath: 'assets/images/intro_customer.png',
+                  onPressed: () {
+                    Navigator.pushNamed(context, MapScreen.routeName);
+                  },
                 ),
               ],
             ),
@@ -104,33 +50,43 @@ class IntroScreen extends StatelessWidget {
   }
 }
 
-class IntroScreenButton extends StatelessWidget {
-  IntroScreenButton({this.label, this.onPressed});
-
+// TODO create clean separate widget in components directory when we decided for a design
+class _IntroScreenButton extends StatelessWidget {
   final String label;
-  final Function() onPressed;
+  final String imagePath;
+  final VoidCallback onPressed;
+
+  _IntroScreenButton({this.label, this.imagePath, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8.0),
-        child: Container(
-          width: double.infinity,
-          color: Color.fromARGB(255, 191, 231, 226),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              this.label,
-              style: TextStyle(
-                fontSize: 48.0,
-                fontStyle: FontStyle.normal,
-                fontWeight: FontWeight.w100,
+    return FlatButton(
+      color: Colors.teal,
+      textColor: Colors.white,
+      padding: EdgeInsets.all(8.0),
+      onPressed: onPressed,
+      child: Row(
+        children: <Widget>[
+          Image(
+            image: AssetImage(imagePath),
+            height: 115,
+            width: 115,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                label,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36.0,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w100,
+                ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
